ee9e2b4d16cb7cf3e0272ce5aa9f54c27e3d5ace
covi-ensil <ensil@covi.co.kr>
2024-06-28 13:05:41 +0900

    버전) 0.2.15: spm 테스트

--- Package.swift
+++ Package.swift
@@ -14,15 +14,26 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.4.0")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser.git", .upToNextMinor(from: "5.3.0")),
-        .package(url: "https://github.com/piemonte/Player.git", .upToNextMinor(from: "0.13.0"))
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.4.0")),
+        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.0.0")),
+        .package(name: "SwiftyXMLParser", url: "https://github.com/yahoojapan/SwiftyXMLParser.git", .upToNextMinor(from: "5.3.0")),
+        .package(name: "Player", url: "https://github.com/piemonte/Player.git", .upToNextMinor(from: "0.13.0"))
     ],
     targets: [
         .binaryTarget(
             name: "covisdk",
             path: "./covisdk.xcframework"
+        ),
+        .target(
+            name: "covisdkWrapper",
+            dependencies: [
+                .target(name: "covisdk"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "SwiftyXMLParser", package: "SwiftyXMLParser"),
+                .product(name: "Player", package: "Player")
+            ],
+            path: "./Sources"
         )
     ],
     swiftLanguageVersions: [.v5]

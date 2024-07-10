4d559a86b9162b96e4461e08536903b0f710ceb9
covi-ensil <ensil@covi.co.kr>
2024-07-10 16:02:44 +0900

    버전) 0.3.2: Alamofire 의존성 제거

--- covisdk.xcframework/ios-arm64_x86_64-simulator/covisdk.framework/Headers/covisdk-Swift.h
+++ covisdk.xcframework/ios-arm64_x86_64-simulator/covisdk.framework/Headers/covisdk-Swift.h
@@ -317,7 +317,6 @@ SWIFT_CLASS("_TtC7covisdk10CoviPlayer")
 
 
 
-
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -638,7 +637,6 @@ SWIFT_CLASS("_TtC7covisdk10CoviPlayer")
 - (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
-
 
 
 

869a7c3633fae026750baf13066dd0aee3dca152
covi-ensil <ensil@covi.co.kr>
2024-07-12 17:15:05 +0900

    버전) 0.5.1: SPM 라이브러리 테스트

--- Package.swift
+++ Package.swift
@@ -9,31 +9,16 @@ let package = Package(
     products: [
         .library(
             name: "covi-pod-test",
-            targets: ["dependencyWrapper"]
+            targets: ["covi-pod-test"]
         )
     ],
     dependencies: [
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.4.0")),
-        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.0.0")),
-        .package(name: "SwiftyXMLParser", url: "https://github.com/yahoojapan/SwiftyXMLParser.git", .upToNextMinor(from: "5.3.0")),
-        .package(name: "Player", url: "https://github.com/piemonte/Player.git", .upToNextMinor(from: "0.13.0"))
     ],
     targets: [
         .binaryTarget(
             name: "covisdk",
             path: "./covisdk.xcframework"
-        ),
-        .target(
-            name: "dependencyWrapper",
-            dependencies: [
-                .target(name: "covisdk", condition: .when(platforms: .some([.iOS]))),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "SDWebImage", package: "SDWebImage"),
-                .product(name: "SwiftyXMLParser", package: "SwiftyXMLParser"),
-                .product(name: "Player", package: "Player"),
-            ],
-            path: "./"
-        ),
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
